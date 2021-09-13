e95841be22507ef4b87ffc8b85134aa771b8447c
alex <alexcarvajal2404@gmail.com>
2021-09-13 09:24:27 -0400

    avances en la parte del home y searcher

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule)
+  },
+  {
+    path: 'bio-security',
+    loadChildren: () => import('./pages/bio-security/bio-security.module').then(m => m.BioSecurityModule)
+  },
+  {
+    path: 'inmuebles',
+    loadChildren: () => import('./pages/inmuebles/inmuebles.module').then(m => m.InmueblesModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
